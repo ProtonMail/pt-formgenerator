c7e12f6b988a2b8eb64ade4a115f704d5d5bf17c
dhoko <aurelien@protonmail.com>
2019-01-22 15:48:29 +0100

    Improve dist

--- src/main.js
+++ src/main.js
@@ -3,36 +3,11 @@ import App from './App';
 
 const node = document.getElementById('app');
 
-const config = [
-    {
-        component: 'username',
-        label: 'Choose a username',
-        placeholder: 'Jean Valjean',
-        maxlength: 10,
-        minlength: 3,
-        required: true,
-        name: 'username'
-    },
-    {
-        component: 'domains',
-        label: 'Select a domain',
-        name: 'domains',
-        options: [
-            {
-                selected: true,
-                value: 'protonmail.com'
-            },
-            {
-                value: 'protonmail.ch'
-            }
-        ]
-    }
-];
-
 const cb = ({ data: { type, data = {} } = {} }) => {
     if (type === 'create.form') {
-        console.log('[CONFIG]', config);
+        console.log('[CONFIG]', data.config);
         render(<App config={data.config} />, node, node.lastChild);
+        node.setAttribute('data-name', data.name);
         window.removeEventListener('message', cb, false);
     }
 };
@@ -41,6 +16,31 @@ window.addEventListener('message', cb, false);
 // window.postMessage({
 //     type: 'create.form',
 //     data: {
-//         config
+//         name: 'dew',
+//         config: [
+//             {
+//                 component: 'username',
+//                 label: 'Choose a username',
+//                 placeholder: 'Jean Valjean',
+//                 maxlength: 10,
+//                 minlength: 3,
+//                 required: true,
+//                 name: 'username'
+//             },
+//             {
+//                 component: 'domains',
+//                 label: 'Select a domain',
+//                 name: 'domain',
+//                 options: [
+//                     {
+//                         selected: true,
+//                         value: 'protonmail.com'
+//                     },
+//                     {
+//                         value: 'protonmail.ch'
+//                     }
+//                 ]
+//             }
+//         ]
 //     }
 // });
