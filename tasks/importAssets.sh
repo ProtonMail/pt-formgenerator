1250c4638b894dd68cf9c1a19eae416adb5b1f8a
dhoko <dhoko@pm.me>
2019-09-05 13:16:01 +0200

    Add missing svg content loader

--- tasks/importAssets.sh
+++ tasks/importAssets.sh
@@ -13,5 +13,6 @@ fi;
 if [ "$1" = 'dist' ]; then
     mkdir -p "dist/$TREE";
     cp "./src/$TREE/loading.gif" "./dist/$TREE/loading.gif";
+    npx svgo "./src/$TREE/loading-atom-smaller.svg" -o "./dist/$TREE/loading-atom-smaller.svg";
     npx svgo "./src/$TREE/sprite-for-css-only.svg" -o "./dist/$TREE/sprite-for-css-only.svg";
 fi;
